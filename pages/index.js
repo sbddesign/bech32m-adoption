e0741566d86a29787cec3a72b2c089d69ca964c1
Stephen DeLorme <stephen@d.elor.me>
2022-08-06 13:33:54 -0400

    Tailwind install

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-4xl font-bold underline">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
