f81cc1cfd1003d79591d7940c9f81eaaf3e1b9cf
Stephen DeLorme <stephen@d.elor.me>
2022-08-08 17:13:11 -0400

    Build fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {BitcoinIcon} from '@bitcoin-design/bitcoin-icons-react/filled'
 import {BeakerIcon} from '@heroicons/react/solid'
